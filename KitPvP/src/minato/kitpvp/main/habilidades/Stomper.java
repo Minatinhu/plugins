5dbb33ae301d9a7408745003537a853d7b485225
Minato Gamer42 <minato.xplayen.tk>
2018-11-28 16:05:03 -0200

    Atualizando

--- KitPvP/src/minato/kitpvp/main/habilidades/Stomper.java
+++ KitPvP/src/minato/kitpvp/main/habilidades/Stomper.java
@@ -2,7 +2,7 @@ package minato.kitpvp.main.habilidades;
 
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,17 +16,24 @@ public class Stomper extends KitMain implements Listener {
 	
 	@EventHandler
 	public void StomperEvent(EntityDamageEvent e) {
-		Player p = (Player) e.getEntity();
-		if(e.getEntity() instanceof Player) {
-			if(kitspvp().get(p) == KitType.STOMPER) {
-				for(Entity p2 : p.getNearbyEntities(3, 3, 3)) {
-					Player p21 = (Player) p2;
-					if(p21 instanceof Player) {
-						p21.damage(e.getDamage());
-						p21.playSound(p21.getLocation(), Sound.ANVIL_LAND, 1f, 1f);
+		if (e.getEntity() instanceof Player) {
+			Player p = (Player) e.getEntity();
+			if (hasKit(p, KitType.STOMPER)) {
+				double d = e.getDamage();
+				for (Entity alvo : p.getNearbyEntities(3, 3, 3)) {
+					if (alvo instanceof Player) {
+						Player pa = (Player) alvo;
+						if (!pa.isSneaking()) {
+							pa.damage(d);
+						}
+					} else if (alvo instanceof LivingEntity) {
+						LivingEntity livingEntity = (LivingEntity) alvo;
+						livingEntity.damage(d);
+
 					}
 				}
-				p.playSound(p.getLocation(), Sound.ANVIL_LAND, 1f, 1f);
+				e.setDamage(d > 2 ? 2 : d);
+				p.playSound(p.getLocation(), Sound.ANVIL_LAND, 1, 1);
 			}
 		}
 	}
