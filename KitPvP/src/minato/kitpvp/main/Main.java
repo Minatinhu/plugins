4ba277a6284e79d8929d6feb0052d9274cb986ab
Minato Gamer42 <minato.xplayen.tk>
2018-11-28 22:45:07 -0200

    Autualizando

--- KitPvP/src/minato/kitpvp/main/Main.java
+++ KitPvP/src/minato/kitpvp/main/Main.java
@@ -1,9 +1,15 @@
 package minato.kitpvp.main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import minato.kitpvp.main.automessage.AutoMessage;
 import minato.kitpvp.main.comandos.Ping;
 import minato.kitpvp.main.eventos.InteractOpen;
 import minato.kitpvp.main.eventos.JoinLeave;
@@ -13,27 +19,50 @@ import minato.kitpvp.main.habilidades.Dilma;
 import minato.kitpvp.main.habilidades.Stomper;
 
 public class Main extends JavaPlugin {
-	
+
 	private static Main instance;
 	public static FileConfiguration cf;
-	
+	public static File autof = new File(getInstance().getDataFolder(), "automessage.yml");
+	public static FileConfiguration auto = YamlConfiguration.loadConfiguration(autof);
+
 	public void onEnable() {
 		cf = getConfig();
 		setInstance(this);
 		newMessage("&2[&cKitPvP] &2Plugin ligado com sucesso!");
-		//Events
+		if (!autof.exists()) {
+			auto.addDefault("Auto.Name", "&9[&bRede&cGet]");
+			auto.addDefault("Auto.Boolean", true);
+			auto.addDefault("Auto.Temp", 1);
+			ArrayList<String> a = new ArrayList<>();
+			a.add("&bBem Vindo a o " + auto.getString("Auto.Name"));
+			auto.addDefault("Auto.List", a);
+			auto.options().copyDefaults(true);
+			try {
+				auto.save(autof);
+				newMessage("&2[&cKitPvP] Arquivo " + autof.getName() + " criado com sucesso!");
+			} catch (IOException e) {
+				newMessage("&c[&cKitPvP] &c Não Foi possivel criar o arquivo" + autof.getName());
+				e.printStackTrace();
+			}
+		}
+		if(autof.exists() == true) {
+			if(auto.getBoolean("Auto.Boolean") == true) {
+				new AutoMessage();
+			}
+		}
+		// Events
 		getServer().getPluginManager().registerEvents(new JoinLeave(), this);
 		getServer().getPluginManager().registerEvents(new InteractOpen(), this);
 		getServer().getPluginManager().registerEvents(new SoupEvent(), this);
 		getServer().getPluginManager().registerEvents(new Jump(), this);
-		
-		//Kits
+
+		// Kits
 		getServer().getPluginManager().registerEvents(new Stomper(), this);
 		getServer().getPluginManager().registerEvents(new Dilma(), this);
-		
-		//Commands
+
+		// Commands
 		getCommand("ping").setExecutor(new Ping());
-		
+
 	}
 
 	public static Main getInstance() {
@@ -43,7 +72,7 @@ public class Main extends JavaPlugin {
 	public static void setInstance(Main instance) {
 		Main.instance = instance;
 	}
-	
+
 	void newMessage(String Message) {
 		getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', Message));
 	}
--- KitPvP/src/minato/kitpvp/main/automessage/AutoMessage.java
+++ KitPvP/src/minato/kitpvp/main/automessage/AutoMessage.java
@@ -1,9 +1,28 @@
 package minato.kitpvp.main.automessage;
 
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import minato.kitpvp.main.Main;
+
 public class AutoMessage {
 	
-	public void rodar() {
-		
+	public AutoMessage() {
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+				for(Player p : Bukkit.getOnlinePlayers()) {
+					Random r = new Random();
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.auto.getStringList("Auto.List").get(r.nextInt())));
+				}
+				
+			}
+		}.runTaskTimer(Main.getInstance(), Main.auto.getInt("Auto.Temp"), Main.auto.getInt("Auto.Temp"));
 	}
 
 }
